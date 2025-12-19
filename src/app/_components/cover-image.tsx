3eead8ace67bcf996a78ed1937234d12a182e0d6
Sging <105583538+gwangseok2@users.noreply.github.com>
2025-12-19 21:16:57 +0900

    [ + ] robinhood add posting

--- src/app/_components/cover-image.tsx
+++ src/app/_components/cover-image.tsx
@@ -12,7 +12,7 @@ const CoverImage = ({ title, src, slug }: Props) => {
   const image = (
     <Image
       src={src}
-      alt={`Cover Image for ${title}`}
+      alt={`${title}`}
       className={cn("shadow-sm w-full", {
         "hover:shadow-lg transition-shadow duration-200": slug,
       })}
